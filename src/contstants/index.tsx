5fcf8496bf02ff1a60b353ffff1078cc1cdff6c1
Mi-Lan <git>
2022-06-02 01:11:39 +0200

    fix: Added static assets

---
+++ src/contstants/index.tsx
@@ -0,0 +1,17 @@
+// import { NimiLinkType } from '../library';
+import LensterLogo from '../assets/socialIcons/lenster.svg';
+import InstagramLogo from '../assets/socialIcons/instagram.svg';
+import TwitterLogo from '../assets/socialIcons/twitter.svg';
+import EmailLogo from '../assets/socialIcons/mail.svg';
+import LinkedInLogo from '../assets/socialIcons/linkedIn.svg';
+import WebsiteLogo from '../assets/socialIcons/website.svg';
+import TelegramLogo from '../assets/socialIcons/telegram.svg';
+export const socialImagesMapping = {
+  twitter: TwitterLogo,
+  lenster: LensterLogo,
+  emai: EmailLogo,
+  instagram: InstagramLogo,
+  linkedin: LinkedInLogo,
+  website: WebsiteLogo,
+  telegram: TelegramLogo,
+};
