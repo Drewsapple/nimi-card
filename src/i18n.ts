e8519a463d42492617d2a33f46e49d2206a3945c
Adam Azad <adam@adamazad.com>
2022-06-16 09:38:30 +0100

    fix: lint

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,9 @@
-import i18next from 'i18next';
+import { use } from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import XHR from 'i18next-xhr-backend';
 import { initReactI18next } from 'react-i18next';
 
-i18next
-  .use(XHR)
+use(XHR)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
@@ -22,5 +21,3 @@ i18next
     load: 'languageOnly',
     defaultNS: 'common',
   });
-
-export default i18next;
