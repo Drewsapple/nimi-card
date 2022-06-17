1ef9f2e8fd522f21e4c424d36a95afd4976660e2
Adam Azad <adam@adamazad.com>
2022-06-17 12:42:22 +0100

    feat: fetch data from data.json

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,77 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect, useState } from 'react';
+import { unstable_batchedUpdates } from 'react-dom';
+import styled from 'styled-components';
 
-import exampleNimi from '../data/nimi-card.json';
+import { ReactComponent as NimiLogoMarkSvg } from '../library/assets/svg/nimi-logo-mark.svg';
 import { Nimi, NimiCard, Container as NimiCardContainer } from '../library/components/NimiCard';
 
+const StyledCenteredContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledNimiLogoMarkSvg = styled(NimiLogoMarkSvg)`
+  width: 100px;
+  width: 25vw;
+  max-width: 200px;
+`;
+
 export function App() {
+  const [nimi, setNimi] = useState<Nimi>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<Error>();
+
+  useEffect(() => {
+    const dataPath = process.env.NODE_ENV === 'production' ? '/data.json' : '/data.json';
+
+    fetch(dataPath, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(async (response) => {
+        if (response.ok) {
+          const json = (await response.json()) as Nimi;
+          unstable_batchedUpdates(() => {
+            setNimi(json);
+            setIsLoading(false);
+          });
+        }
+      })
+      .catch((error) => {
+        unstable_batchedUpdates(() => {
+          setIsLoading(false);
+          setError(error);
+          console.error(error);
+        });
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <StyledCenteredContainer>
+        <StyledNimiLogoMarkSvg />
+      </StyledCenteredContainer>
+    );
+  }
+
+  if (error || !nimi) {
+    return (
+      <StyledCenteredContainer>
+        <StyledNimiLogoMarkSvg />
+        <p>Something went wrong.</p>
+      </StyledCenteredContainer>
+    );
+  }
+
   return (
     <Suspense fallback={null}>
       <NimiCardContainer>
-        <NimiCard nimi={exampleNimi as Nimi} />
+        <NimiCard nimi={nimi} />
       </NimiCardContainer>
     </Suspense>
   );
