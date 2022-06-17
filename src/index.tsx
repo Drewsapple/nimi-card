4331793e62235771c6a2b2a6564d23e2c306219b
Adam Azad <adam@adamazad.com>
2022-06-17 15:37:20 +0100

    chore: remove i18n

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 
 import { App } from './pages/App';
 import { FixedGlobalStyle, ThemedGlobalStyle, ThemeProvider } from './theme';
-import './i18n';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
