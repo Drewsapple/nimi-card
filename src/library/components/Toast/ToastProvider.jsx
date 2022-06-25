9fcd4c9c2b34efc288eb0b7d5c862fbd33b43f34
Adam Azad <adam@adamazad.com>
2022-06-25 23:47:26 +0100

    feat(toast): use nanoid

--- src/library/components/Toast/ToastProvider.jsx
+++ src/library/components/Toast/ToastProvider.jsx
@@ -1,3 +1,4 @@
+import { nanoid } from 'nanoid';
 import { useMemo, useState } from 'react';
 import { createPortal } from 'react-dom';
 
@@ -5,19 +6,9 @@ import { StyledToastWrapper } from './styled';
 import { Toast } from './Toast';
 import { ToastContext } from './ToastContext';
 
-// Create a random ID
-function generateUEID() {
-  let first = (Math.random() * 46656) | 0;
-  let second = (Math.random() * 46656) | 0;
-  first = ('000' + first.toString(36)).slice(-3);
-  second = ('000' + second.toString(36)).slice(-3);
-
-  return first + second;
-}
-
 export const ToastProvider = (props) => {
   const [toasts, setToasts] = useState([]);
-  const open = (content) => setToasts((currentToasts) => [...currentToasts, { id: generateUEID(), content }]);
+  const open = (content) => setToasts((currentToasts) => [...currentToasts, { id: nanoid, content }]);
   const close = (id) => setToasts((currentToasts) => currentToasts.filter((toast) => toast.id !== id));
   const contextValue = useMemo(() => ({ open }), []);
 
