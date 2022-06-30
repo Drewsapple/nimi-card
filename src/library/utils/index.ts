8d7d0ad806fbaaff502e5b4e84bf52e3d742b779
Mi-Lan <git>
2022-06-30 22:37:37 +0200

    fix: Moved is valid url to library -.-

--- src/library/utils/index.ts
+++ src/library/utils/index.ts
@@ -1,5 +1,19 @@
-import { isValidUrl } from '../../utils/isValidUrl';
 import { NimiBlockchain, nimiBlockchainDetails, NimiLinkBaseDetails } from '../components/NimiCard/types';
+/**
+ * Returns true if value is proper url
+ * @param urlString
+ */
+export function isValidUrl(urlString: string): boolean {
+  let url: URL;
+
+  try {
+    url = new URL(urlString);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === 'http:' || url.protocol === 'https:';
+}
 
 /**
  * Given a blockchain and address, return the explorer url
